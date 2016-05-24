3d549ecb5e6d7afc92e9fc2f2f9d419cec4b47d3
loveKino <842914439@qq.com>
2016-05-24 12:47:52 +0800

    stage-local

--- language/c/computerSystemsAProgrammer'sPerspective/chapter2/informationStorage.c
+++ language/c/computerSystemsAProgrammer'sPerspective/chapter2/informationStorage.c
@@ -53,6 +53,29 @@ void practice2D7 () {
     printf("%x\n", x & ~0xFF); // 0x98FDEC00
 }
 
+void practice2D8 () {
+    int x = 0b00110011;
+    int m = 0b11110000;
+    // bic
+    printf("0x%x\n", x & m);
+    // bis
+    printf("0x%x\n", x | m);
+}
+
+void practice2D10 () {
+    int x = 0b00101001;
+    int y = 0b00101011;
+    int z = 0b00101001;
+    printf("0x%x\n", !(x ^ y));
+    printf("0x%x\n", !(x ^ z));
+}
+
+void practice2D11 () {
+    int x = 0x55;
+    printf("0x%x\n", x << 3);
+    printf("0x%x\n", x >> 2);
+}
+
 int main(void) {
     test_show_bytes(0x12345678);
     practice2D2();
@@ -63,4 +86,7 @@ int main(void) {
     inplace_swap(&a, &b);
     printf("a=%d, b=%d\n", a, b);
     practice2D7();
+    practice2D8();
+    practice2D10();
+    practice2D11();
 }
